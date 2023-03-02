6be0ea47ad02d16880e9a6257f307bd0629e2cbe
szhcoco <122571594+szhcoco@users.noreply.github.com>
2023-03-02 13:06:53 -0800

    Add files via upload

--- grade.sh
+++ grade.sh
@@ -4,7 +4,26 @@ rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
 
-cp student-submission/ListExamples.java ./
-javac -cp $CPATH *.java
-java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
+cd student-submission
+if [[ -f ListExamples.java ]]
+then 
+    echo 'We found the file'
+else
+    echo 'We did not find it'
+fi
 
+cp ../TestListExamples.java ./
+cp -r ../lib ./
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+RESULT = 'echo $?'
+if [[ $RESULT -eq 0 ]]
+then 
+    echo 'Compile successfully'
+else 
+    RESULT > out.txt 2>&1
+    cat out.txt
+fi
+
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples 2> error.txt
+
+cat error.txt
